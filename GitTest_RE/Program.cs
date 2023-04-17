56c124e1b1c22795f1185a6d6e0f98b643c65735
Jung BoBae <acarid27@gmail.com>
2023-04-17 20:13:53 +0900

    초기 커밋.

---
+++ GitTest_RE/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitTest_RE
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
