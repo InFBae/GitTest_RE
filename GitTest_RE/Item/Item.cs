1b49dec8737862216ef740d9f28b81bb85296e6a
Jung BoBae <acarid27@gmail.com>
2023-04-17 21:35:40 +0900

    fix: 아이템, 미사일 수정
    아이템과 미사일 클래스 수정

--- GitTest_RE/Item/Item.cs
+++ GitTest_RE/Item/Item.cs
@@ -9,13 +9,15 @@ namespace GitTest_RE.Item
     internal class Item
     {
         // 아이템 생성
-        protected Item()
-        {
+        private String name { get; set; }
 
+        public Item()
+        {
+            name = "아이템";
         }
         protected void UseItem()
         {
-            Console.WriteLine("아이템을 사용했습니다.");
+            Console.WriteLine($"{name}을 사용했습니다.");
         }
     }
 }
--- GitTest_RE/Item/Missile.cs
+++ GitTest_RE/Item/Missile.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace GitTest_RE.Item
 {
-    internal class Missile
+    internal class Missile : Item
     {
+        public Missile() {
+            name = "미사일";
+        }
+
     }
 }
