63ee746a76777659b7ed43eaae294f5da5486629
jim raynor <fallere82531@gmail.com>
2023-04-17 20:30:47 +0900

    feat: Create cart class

---
+++ GitTest_RE/Cart.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest_RE
+{
+    public abstract class Cart
+    {
+        public abstract int drift { get; set; }
+        public abstract int acceleration { get; set; }
+        public abstract int cornering { get; set; }
+        public abstract int boosterTime { get; set; }
+        public abstract int boosterCharge { get; set; }
+    }
+}
