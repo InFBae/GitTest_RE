ae716a00212fca3de02783952ab68a7432410614
Jung BoBae <acarid27@gmail.com>
2023-04-17 21:05:06 +0900

    feat: 빌리지 손가락 맵 생성
    빌리지 손가락 맵 생성

---
+++ GitTest_RE/Map/VillageFinger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest_RE.Map
+{
+    internal class VillageFinger :Map
+    {
+        public VillageFinger() { Name = "빌리지손가락"; }
+    }
+}
