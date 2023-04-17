043576c1debd057b1de015d60ab23cc49ed13026
YongJoon_Kim <be2163@korea.ac.kr>
2023-04-17 21:01:22 +0900

    fix. Fix Functions virtual to none

--- GitTest_RE/Character.cs
+++ GitTest_RE/Character.cs
@@ -11,11 +11,11 @@ namespace GitTest_RE
         protected string name;
         protected string charFileURL;
 
-        protected virtual void setName(string name)
+        protected void setName(string name)
         {
             this.name = name;
         }
-        protected virtual void setCharFileURL(string name)
+        protected void setCharFileURL(string name)
         {
             this.charFileURL = name;
         }
