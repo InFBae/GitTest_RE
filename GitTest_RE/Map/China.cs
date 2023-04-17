0ad992a8c1d040b9727f3ac4096b1bd3741edc53
Jung BoBae <acarid27@gmail.com>
2023-04-17 21:46:51 +0900

    feat: 차이나 맵 추가
    차이나 맵 추가

---
+++ GitTest_RE/Map/China.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTest_RE.Map
+{
+    internal class China : Map
+    {
+        public China() { Name = "차이나"; }
+    }
+}
